9dde9c24c3be1622a659018932235ed4f26482d7
atomic <baileyweb04@gmail.com>
2024-03-20 13:31:06 -0400

    test

--- twitter-clone/src/pages/Settings.jsx
+++ twitter-clone/src/pages/Settings.jsx
@@ -1,6 +1,24 @@
 function Settings() {
-  return(
-    <h1>Settings</h1>
+  return (
+    <div className="settings-container">
+      <h1>Settings</h1>
+      <div className="settings-side-bar">
+        <div className="settings-side-bar-top">
+          <input type="text" placeholder="Search Settings" />
+        </div>
+        <div className="serrints-side-bar-mid">
+          <ul>
+            <li><h1>Your account</h1></li>
+            <li><h1>Monetization</h1></li>
+            <li><h1>Subscriptions</h1></li>
+            <li><h1>Security</h1></li>
+            <li><h1>Privacy</h1></li>
+            <li><h1>Notfifications</h1></li>
+            <li><h1>Accessibilty & Langauges</h1></li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 };
 
