c95609041d049d4d7368662037b6795481fb86fb
atomic <baileyweb04@gmail.com>
2024-03-21 01:20:39 -0400

    test

--- twitter-clone/src/App.jsx
+++ twitter-clone/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Logo from './assets/icons/logo.svg';
 import HomeIcon from './assets/icons/home.svg';
 import ExploreIcon from './assets/icons/explore.svg';
@@ -7,7 +8,7 @@ import MessagesIcon from './assets/icons/messages.svg';
 import moreIcon from './assets/icons/more.svg';
 import dotsPfpIcon from './assets/icons/dots.svg';
 import './index.css';
-import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, NavLink, useLocation } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Settings from './pages/Settings';
 import Explore from './pages/Explore';
@@ -17,78 +18,88 @@ import Support from './pages/Support';
 import { RightSideBar, FollowSuggest } from './componets/rightSideBar';
 import { MainProfile } from './componets/storage/profileInfo';
 
-//Display's the navbar and sidepanels
+// Display's the navbar and sidepanels
 function App() {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+}
+
+function AppContent() {
+  const location = useLocation();
 
   const navigateToProfile = () => {
     window.location.href = '/Profile';
   };
 
+  const showRightSideBar = location.pathname === '/' || location.pathname === '/Profile' ||  location.pathname === '/Explore';
+
   return (
-    <Router>
-      <div className='main-container'>
-        <div className="nav">
-          <nav>
-            <div className="nav-inner-content">
-              <div className="nav-top-bar">
-                <img src={Logo} alt="Logo" />
+    <div className='main-container'>
+      <div className="nav">
+        <nav>
+          <div className="nav-inner-content">
+            <div className="nav-top-bar">
+              <img src={Logo} alt="Logo" />
+            </div>
+            <ul>
+              <div className="nav-item">
+                <img src={HomeIcon} alt="Home" />
+                <li><NavLink to="/" style={({ isActive }) => ({ color: isActive ? '#0088ff' : 'black' })}>Home</NavLink ></li>
               </div>
-              <ul>
-                <div className="nav-item">
-                  <img src={HomeIcon} alt="Home" />
-                  <li><NavLink to="/" style={({ isActive }) => ({ color: isActive ? '#0088ff' : 'black' })}
-                  >Home</NavLink ></li>
-                </div>
-                <div className="nav-item">
-                  <img src={ExploreIcon} alt="Explore" />
-                  <li ><NavLink to="/Explore" style={({ isActive }) => ({ color: isActive ? '#0088ff' : 'black' })}>Explore</NavLink ></li>
-                </div>
-                <div className="nav-item">
-                  <img src={BookmarkIcon} alt="Market" />
-                  <li><NavLink to="/Market" style={({ isActive }) => ({ color: isActive ? '#0088ff' : 'black' })}>Market</NavLink ></li>
-                </div>
-                <div className="nav-item">
-                  <img src={MessagesIcon} alt="Messages" />
-                  <li><NavLink to="/Messages" style={({ isActive }) => ({ color: isActive ? '#0088ff' : 'black' })}>Messages</NavLink ></li>
-                </div>
-                <div className="nav-item">
-                  <img src={ProfileIcon} alt="Profile" />
-                  <li><NavLink to="/Profile" style={({ isActive }) => ({ color: isActive ? '#0088ff' : 'black' })}>Profile</NavLink ></li>
-                </div>
-                <div className="nav-item">
-                  <img src={moreIcon} alt="More" />
-                  <li><NavLink to="/Settings" style={({ isActive }) => ({ color: isActive ? '#0088ff' : 'black' })}>Settings</NavLink ></li>
-                </div>
-              </ul>
-            </div>
-            <div className="nav-btn">
-              <button className='btn btn-primary'>Tweet</button>
-            </div>
-          </nav>
-          <div className="nav-bottom-bar">
-            <img src={MainProfile.ProfilePic} alt="pfp" onClick={navigateToProfile} />
-            <div className="nav-bottom-bar-text">
-              <h1 onClick={navigateToProfile}>{MainProfile.ProfileRealName}</h1>
-              <h1 onClick={navigateToProfile}>{MainProfile.ProfileUserName}</h1>
-            </div>
-            <img src={dotsPfpIcon} alt="settings" />
+              <div className="nav-item">
+                <img src={ExploreIcon} alt="Explore" />
+                <li ><NavLink to="/Explore" style={({ isActive }) => ({ color: isActive ? '#0088ff' : 'black' })}>Explore</NavLink ></li>
+              </div>
+              <div className="nav-item">
+                <img src={BookmarkIcon} alt="Market" />
+                <li><NavLink to="/Market" style={({ isActive }) => ({ color: isActive ? '#0088ff' : 'black' })}>Market</NavLink ></li>
+              </div>
+              <div className="nav-item">
+                <img src={MessagesIcon} alt="Messages" />
+                <li><NavLink to="/Messages" style={({ isActive }) => ({ color: isActive ? '#0088ff' : 'black' })}>Messages</NavLink ></li>
+              </div>
+              <div className="nav-item">
+                <img src={ProfileIcon} alt="Profile" />
+                <li><NavLink to="/Profile" style={({ isActive }) => ({ color: isActive ? '#0088ff' : 'black' })}>Profile</NavLink ></li>
+              </div>
+              <div className="nav-item">
+                <img src={moreIcon} alt="More" />
+                <li><NavLink to="/Settings" style={({ isActive }) => ({ color: isActive ? '#0088ff' : 'black' })}>Settings</NavLink ></li>
+              </div>
+            </ul>
           </div>
+          <div className="nav-btn">
+            <button className='btn btn-primary'>Tweet</button>
+          </div>
+        </nav>
+        <div className="nav-bottom-bar">
+          <img src={MainProfile.ProfilePic} alt="pfp" onClick={navigateToProfile} />
+          <div className="nav-bottom-bar-text">
+            <h1 onClick={navigateToProfile}>{MainProfile.ProfileRealName}</h1>
+            <h1 onClick={navigateToProfile}>{MainProfile.ProfileUserName}</h1>
+          </div>
+          <img src={dotsPfpIcon} alt="settings" />
         </div>
-        <Routes>
-          <Route exact path="/" element={<Landing />} />
-          <Route path="/Explore" element={<Explore />} />
-          <Route path="/Messages" element={<Messages />} />
-          <Route path="/Market" element={<Support />} />
-          <Route path="/Profile" element={<Profile />} />
-          <Route path="/Settings" element={<Settings />} />
-        </Routes>
+      </div>
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        <Route path="/Explore" element={<Explore />} />
+        <Route path="/Messages" element={<Messages />} />
+        <Route path="/Market" element={<Support />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/Settings" element={<Settings />} />
+      </Routes>
+      {showRightSideBar && (
         <div className="right-side-bar">
           <RightSideBar />
           <FollowSuggest />
         </div>
-      </div>
-    </Router>
+      )}
+    </div>
   );
 }
 
-export default App
+export default App;
--- twitter-clone/src/pages/Settings.jsx
+++ twitter-clone/src/pages/Settings.jsx
@@ -1,21 +1,38 @@
+import rightArrow from '../assets/icons/right-arrow.svg';
+
 function Settings() {
   return (
     <div className="settings-container">
       <h1>Settings</h1>
       <div className="settings-side-bar">
         <div className="settings-side-bar-top">
-          <input type="text" placeholder="Search Settings" />
+          <input type="text" placeholder="&#x1F50E; Search Settings" />
         </div>
-        <div className="serrints-side-bar-mid">
-          <ul>
-            <li><h1>Your account</h1></li>
-            <li><h1>Monetization</h1></li>
-            <li><h1>Subscriptions</h1></li>
-            <li><h1>Security</h1></li>
-            <li><h1>Privacy</h1></li>
-            <li><h1>Notfifications</h1></li>
-            <li><h1>Accessibilty & Langauges</h1></li>
-          </ul>
+        <div className="settings-side-bar-mid">
+          <div className="settings-item">
+            <h1>Your account</h1>
+            <img src={rightArrow} alt="Settings" />
+          </div>
+          <div className="settings-item">
+            <h1>Security</h1>
+            <img src={rightArrow} alt="Settings" />
+          </div>
+          <div className="settings-item">
+            <h1>Privacy</h1>
+            <img src={rightArrow} alt="Settings" />
+          </div>
+          <div className="settings-item">
+            <h1>Notifications</h1>
+            <img src={rightArrow} alt="Settings" />
+          </div>
+          <div className="settings-item">
+            <h1>Billing</h1>
+            <img src={rightArrow} alt="Settings" />
+          </div>
+          <div className="settings-item">
+            <h1>Accessibilty</h1>
+            <img src={rightArrow} alt="Settings" />
+          </div>
         </div>
       </div>
     </div>
